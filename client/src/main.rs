094e2f5b5e7514d1d8f7e25f6b7165ec66b751f9
Luiz Roberto Lethang Rodolpho <luiz@portaltelemedicina.com.br>
2023-02-07 14:38:16 -0300

    Implement stream writer arc mutex with concurreny.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,44 +1,42 @@
+use std::sync::Arc;
+
 use fltk::{
-    app::{App, self},
-    window::Window,
+    app::{self, App},
     enums::Event,
-    prelude::*
+    prelude::*,
+    window::Window,
 };
-use tokio::{
-    io::{AsyncWriteExt },
-    net::{TcpStream},
-};
-async fn send_message(mut stream: TcpStream, msg: String) -> Result<(), Box<dyn std::error::Error>> {
-    stream.write_all(msg.as_bytes()).await?;
-    Ok(())
-}
+use tokio::{io::AsyncWriteExt, net::TcpStream, sync::Mutex};
 
 #[tokio::main]
 async fn main() {
     // Connect to a peer
-    let mut stream = TcpStream::connect("127.0.0.1:5000").await.unwrap();
+    let stream = TcpStream::connect("127.0.0.1:5000").await.unwrap();
 
     let app = App::default();
     let mut window = Window::new(20, 20, 800, 600, "Moonlight");
-    
+
     window.end();
     window.show();
 
-    window.handle(move |widget, ev: Event| {
-        match ev {
-            Event::Move => {                
-                println!("Pushed! {}", app::event_coords().0);
-                println!("Pushed! {}", app::event_coords().1);
+    let stream_clone = Arc::new(Mutex::new(stream));
 
-                let (_, mut writer) = stream.split();
-                let msg = format!("{}.{}\n", app::event_coords().0, app::event_coords().1);
-                writer.write_all(msg.as_bytes()); // make blocking
+    window.handle(move |_widget, ev: Event| {
+        match ev {
+            Event::Move => {
+                let msg = format!("({}.{})\n", app::event_coords().0, app::event_coords().1);
+                println!("POS:! {}", msg);
+
+                let stream_arc = Arc::clone(&stream_clone);
+                tokio::spawn(async move {
+                    let mut lock = stream_arc.lock().await;
+                    lock.write_all(msg.as_bytes()).await.unwrap();
+                });
                 true
-            },
+            }
             /* other events to be handled */
             _ => false,
         }
     });
     app.run().unwrap();
 }
-
