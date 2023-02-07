3ee9299a023bf81a442914fb5e02b17c9f524586
Luiz Roberto Lethang Rodolpho <luiz@portaltelemedicina.com.br>
2023-02-06 22:55:44 -0300

    Implement basic message sent by the client.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{
+    io::{AsyncWriteExt },
+    net::{TcpStream}
+};
+
+#[tokio::main]
+async fn main() {
+    // Connect to a peer
+    let mut connection = TcpStream::connect("127.0.0.1:5000").await.unwrap();
+
+    tokio::spawn(async move {
+        let (_, mut writer) = connection.split();
+        let msg = &"aaaaaa\n";
+        writer.write_all(msg.as_bytes()).await.unwrap();
+    })
+    .await
+    .unwrap();
 }
