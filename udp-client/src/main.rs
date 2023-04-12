7f84de06e9551d7a84fa80d81d07a216e3a1c972
Luiz Roberto Lethang Rodolpho <luiz@portaltelemedicina.com.br>
2023-04-12 17:02:20 -0300

    Include UI elements. Create separated functions for socket management and rendering.

--- udp-client/src/main.rs
+++ udp-client/src/main.rs
@@ -1,40 +1,40 @@
-use std::{sync::Arc, net::SocketAddr};
-use fltk::{
-    app::{self, App},
-    enums::Event,
-    window::Window, prelude::{WidgetBase, WidgetExt, GroupExt},
+use std::{
+    sync::Arc,
+    net::SocketAddr
 };
-use rand::prelude::*;
+use fltk::{
+    app::{self},
+    enums::Event,
+    window::Window, prelude::{WidgetBase, WidgetExt, GroupExt}, frame::Frame, button::Button, text::TextEditor, input::Input,
+};
+use rand::{prelude::*, Error};
 use tokio::{io, net::UdpSocket};
 use moonlight_structs::{self, moonlight_structs::{Player, Message, Messaging}};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let sock = UdpSocket::bind("127.0.0.1:5001".parse::<SocketAddr>().unwrap()).await?;
-    let remote_addr = "127.0.0.1:5000";
-    let r = Arc::new(sock);
-    let s = r.clone();
-    s.connect(remote_addr).await?;
-    let mut buf = [0; 1024];
+    let s = connect().await.unwrap();
 
-    tokio::spawn(async move {
-        loop
-        {
-            let (len, addr) = r.recv_from(&mut buf).await.unwrap();
-            let rec_msg = Message::deserialize_moon(buf[..len].to_vec());
-            // create fltk draw function
+    main_render(s);
 
-        }
-    });
+    Ok(())
+}
 
+fn main_render(s: Arc<UdpSocket>)
+{
     let mut rng = rand::thread_rng();
     let client_id: u32 = rng.gen();
 
-    let app = App::default();
-    let mut window = Window::new(20, 20, 800, 600, "Moonlight");
+    let app = app::App::default().with_scheme(app::Scheme::Gleam);
 
-    window.end();
-    window.show();
+    let mut window = Window::new(20, 20, 800, 600, "Moonlight");
+    let mut frame = Frame::new(0, 0, 400, 200, "");
+    let mut player_name_input = Input::new(100, 20, 80, 40, "Player Name");
+    let mut port_settings_input = Input::new(230, 20, 80, 40, "Port", );
+    let mut connect_button = Button::new(330, 20, 80, 40, "Connect");
+
+    connect_button.set_callback(move |_| (frame.set_label("Hello World!"))); 
+
     window.handle(move |_widget, ev: Event| {
         match ev {
             Event::Move => {
@@ -91,8 +91,31 @@ async fn main() -> io::Result<()> {
             _ => false,
         }
     });
-    
-    app.run().unwrap();
 
-    Ok(())
+    window.end();
+    window.show();
+
+    app.run().unwrap();
+}
+
+async fn connect() -> Result<Arc<UdpSocket>, Error>
+{
+    let sock = UdpSocket::bind("127.0.0.1:5002".parse::<SocketAddr>().unwrap()).await.unwrap();
+    let remote_addr = "127.0.0.1:5000";
+    let r = Arc::new(sock);
+    let s = r.clone();
+    s.connect(remote_addr).await.unwrap();
+    let mut buf = [0; 1024];
+
+    tokio::spawn(async move {
+        loop
+        {
+            let (len, addr) = r.recv_from(&mut buf).await.unwrap();
+            let rec_msg = Message::deserialize_moon(buf[..len].to_vec());
+            // create fltk draw function
+
+        }
+    });
+
+    return Ok(s);
 }
