712551ba995170e5c74563ff0ffdeebb746a2ebc
Luiz Roberto Lethang Rodolpho <luiz@portaltelemedicina.com.br>
2023-04-14 00:54:58 -0300

    Implements message propagation and redrawing.

--- udp-client/src/main.rs
+++ udp-client/src/main.rs
@@ -1,38 +1,43 @@
 use fltk::{
-    app::{self},
+    app::{self, redraw},
     button::Button,
     draw,
-    enums::{self, Event, Key},
+    enums::{self, Event, Key, Color},
     frame::Frame,
     input::Input,
     prelude::{GroupExt, InputExt, WidgetBase, WidgetExt},
     window::Window,
+    valuator,
 };
 use moonlight_structs::{
     self,
     moonlight_structs::{Message, Messaging, Player},
 };
 use rand::{prelude::*, Error};
-use std::{cell::RefCell, net::SocketAddr, rc::Rc, sync::Arc};
-use tokio::{io, net::UdpSocket, runtime::Runtime, sync::Mutex};
+use std::{cell::RefCell, net::SocketAddr, rc::Rc, sync::Arc, time::Duration, borrow::Borrow};
+use tokio::{io, net::UdpSocket, runtime::Runtime, sync::Mutex, time};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let s = connect(5001).await.unwrap();
+    let app = app::App::default().with_scheme(app::Scheme::Gleam);
+    let window = Window::new(20, 20, 800, 600, "Moonlight");
+    let arc_window = Arc::new(Mutex::new(window.clone()));
 
-    main_render(s);
+    let mut rng = thread_rng();
+    let num = rng.gen_range(5000..6000);
+    let s = connect(num, arc_window).await.unwrap();
+
+    main_render(s, app, window);
 
     Ok(())
 }
 
-fn main_render(socket: Arc<UdpSocket>) {
+fn main_render(socket: Arc<UdpSocket>, app: app::App, mut window: Window) {
     let socket2 = socket.clone();
     let mut rng = rand::thread_rng();
     let client_id: u32 = rng.gen();
 
-    let app = app::App::default().with_scheme(app::Scheme::Gleam);
 
-    let mut window = Window::new(20, 20, 800, 600, "Moonlight");
     let mut frame = Frame::new(0, 0, 400, 200, "");
     let player_name_input = Input::new(100, 20, 80, 40, "Player Name");
     let port_settings_input = Input::new(230, 20, 80, 40, "Port");
@@ -152,14 +157,6 @@ fn main_render(socket: Arc<UdpSocket>) {
         }
     });
 
-    // draw something
-    let paddle_pos = Rc::from(RefCell::from(320)); // paddle's starting x position
-    let paddle_c = paddle_pos.clone();
-    window.draw(move |_| {
-        draw::set_draw_color(enums::Color::White);
-        draw::draw_rectf(*paddle_c.borrow(), 540, 160, 20);
-    });
-
     window.end();
     window.show();
 
@@ -200,27 +197,27 @@ pub trait ClientTrait {
     // fn disconnect(&mut self);
 }
 
-impl ClientTrait for Client {
-    fn new() -> Self {
-        let rt = Runtime::new().unwrap();
-        Client {
-            is_connected: false,
-            socket: rt.block_on(connect(5001)).unwrap(),
-        }
-    }
-    fn connect(&mut self, client_port: u16) {
-        let rt = Runtime::new().unwrap();
-        let result = rt.block_on(connect(client_port));
+// impl ClientTrait for Client {
+//     fn new() -> Self {
+//         let rt = Runtime::new().unwrap();
+//         Client {
+//             is_connected: false,
+//             socket: rt.block_on(connect(5001)).unwrap(),
+//         }
+//     }
+//     fn connect(&mut self, client_port: u16) {
+//         let rt = Runtime::new().unwrap();
+//         let result = rt.block_on(connect(client_port));
 
-        self.socket = result.unwrap();
-        self.is_connected = true;
-    }
-    // fn disconnect(&mut self) {
-    //     self.is_connected = false;
-    // }
-}
+//         self.socket = result.unwrap();
+//         self.is_connected = true;
+//     }
+//     // fn disconnect(&mut self) {
+//     //     self.is_connected = false;
+//     // }
+// }
 
-async fn connect(client_port: u16) -> Result<Arc<UdpSocket>, Error> {
+async fn connect(client_port: u16, mut window: Arc<Mutex<Window>>) -> Result<Arc<UdpSocket>, Error> {
     let local_address = format!("127.0.0.1:{}", client_port);
     let remote_address = "127.0.0.1:5000";
     let socket = UdpSocket::bind(local_address.parse::<SocketAddr>().unwrap())
@@ -231,15 +228,77 @@ async fn connect(client_port: u16) -> Result<Arc<UdpSocket>, Error> {
     udp_socket.connect(remote_address).await.unwrap();
     let mut buf = [0; 1024];
 
+    // receive server messages
     tokio::spawn(async move {
+        let mut rect_x = 50;
+        let mut rect_y = 50;
+        let rect_width = 100;
+        let rect_height = 100;
+
         loop {
             let (len, addr) = socket_arc.recv_from(&mut buf).await.unwrap();
             let rec_msg = Message::deserialize_moon(buf[..len].to_vec());
-            // create fltk draw function
 
-            println!("server returned: {:?}", rec_msg);
+            // create fltk draw function
+            let mut locked_window = window.lock().await;
+            
+            let mut square = Square { x: 50, y: 50, size: 50 };
+
+            locked_window.draw(move |_| {
+                draw::set_draw_color(Color::Blue);
+                draw::draw_rect(rec_msg.pos_x, rec_msg.pos_y, rect_width, rect_height);
+            });
+            locked_window.redraw();
+
+            println!("server confirmed message");
         }
     });
+    let udp_clone = udp_socket.clone();
+    // heartbeat
+    // tokio::spawn(async move {
+    //     loop {
+    //         let heartbeat_message = Message {
+    //             message_id: 1,
+    //             message_type: 0,
+    //             player: Player {
+    //                 player_id: 0,
+    //                 player_name: "betolino".to_string(),
+    //             },
+    //             pos_x: 900,
+    //             pos_y: 900,
+    //         };
+    //         let heartbeat = heartbeat_message.serialize_moon();
+    //         let _ = udp_clone.send(&heartbeat).await;
+    //         time::sleep(time::Duration::from_secs(2)).await;
+    //     }
+    // });
 
     return Ok(udp_socket);
 }
+
+
+// DRAWING
+#[repr(i32)]
+#[derive(Copy, Clone)]
+enum Direction {
+    Positive = 1,
+    Negative = -1,
+}
+
+struct Ball {
+    wid: valuator::FillDial,
+    pos: (i32, i32),             // x and y positions
+    dir: (Direction, Direction), // x and y directions
+}
+struct Square {
+    x: i32,
+    y: i32,
+    size: i32,
+}
+
+// impl d for Square {
+//     fn draw(&self) {
+//         drawing::set_draw_color(drawing::Color::from_rgb(255, 0, 0));
+//         drawing::draw_rect(self.x, self.y, self.size, self.size);
+//     }
+// }
--- udp-server/src/main.rs
+++ udp-server/src/main.rs
@@ -1,13 +1,19 @@
-use std::{sync::Arc, net::SocketAddr};
+use std::{sync::Arc, net::SocketAddr, collections::HashMap, rc::Rc};
 
-use moonlight_structs::moonlight_structs::{Message, Messaging};
+use moonlight_structs::moonlight_structs::{Message, Messaging, Player};
 use tokio::{net::UdpSocket, io, sync::mpsc};
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sock = UdpSocket::bind("127.0.0.1:5000".parse::<SocketAddr>().unwrap()).await?;
+
+    // HashMap to keep track of connected clients
+    let mut clients: HashMap<SocketAddr, bool> = HashMap::new();
+
     let r = Arc::new(sock);
     let s = r.clone();
+    let s2 = r.clone();
+
     let (tx, mut rx) = mpsc::channel::<(Vec<u8>, SocketAddr)>(1_000);
 
     tokio::spawn(async move {
@@ -16,19 +22,72 @@ async fn main() -> io::Result<()> {
 
             // create function to handle message
 
-            let len = s.send_to(&rec_msg.serialize_moon(), &addr).await.unwrap();
+            let len = s2.send_to(&rec_msg.serialize_moon(), &addr).await.unwrap();
             println!("server {:?} bytes sent", len);
+            
         }
     });
 
-    let mut buf = [0; 1024];
+    // let mut buf = [0; 1024];
+    // loop {
+    //     let (len, addr) = r.recv_from(&mut buf).await?;
+    //     println!("server {:?} bytes received from {:?}", len, addr);
+    //     // let msg = String::from_utf8_lossy(&buf[..len]);
+    //     // println!("server received: {}", msg);
+    //     let rec_msg = Message::deserialize_moon(buf[..len].to_vec());
+
+    //     tx.send((buf[..len].to_vec(), addr)).await.unwrap();
+    // }
+
     loop {
-        let (len, addr) = r.recv_from(&mut buf).await?;
-        println!("server {:?} bytes received from {:?}", len, addr);
-        // let msg = String::from_utf8_lossy(&buf[..len]);
-        // println!("server received: {}", msg);
+        let mut buf = [0; 1024];
+        let (len, src) = r.recv_from(&mut buf).await?;
+        println!("Received {} bytes from {}", len, src);
+
+        // Add the source address to the set of clients.
+        clients.insert(src, true);
+        let mut arc_clients = Box::new(clients.clone());
+
+        // Handle the received data.
         let rec_msg = Message::deserialize_moon(buf[..len].to_vec());
 
-        tx.send((buf[..len].to_vec(), addr)).await.unwrap();
+        // Poll the HashMap to see if any clients have disconnected
+        let mut disconnected_clients = Vec::new();
+
+        // If necessary, remove disconnected clients.
+        for (client, connected) in clients.clone() {
+            // let ping_message = ping_message().serialize_moon();
+            //match s.send_to(&ping_message, client).await {
+            match s.send_to(&buf[..len].to_vec(), client).await {
+                Ok(_size) => {
+                    println!("Sent ping to {}, size {}", client, _size);
+                }
+                Err(_) => {
+                    // Client is no longer connected
+                    disconnected_clients.push(client);
+                }
+            }
+        }
+
+        // Remove disconnected clients from the HashMap
+        for dc in disconnected_clients {
+            arc_clients.remove(&dc);
+            println!("Client {} disconnected.", dc);
+        }
     }
+}
+
+fn ping_message() -> Message {
+    let message = Message {
+        message_id: 0,
+        message_type: 10,
+        player: Player {
+            player_id: 0,
+            player_name: "admin".to_string(),
+        },
+        pos_x: 0,
+        pos_y: 0
+    };
+
+    return message;
 }
